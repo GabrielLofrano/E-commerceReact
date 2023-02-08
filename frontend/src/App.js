b1ed61135d555d6e4f076cb0e1515177ccd4f9bd
GabrielLofrano <gabrielofrano@hotmail.com>
2023-02-08 12:22:48 -0300

    Adicionando lista de produtos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,32 @@
-
+import data from "./data";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="/">Amazona</a>
+        <a href="/">Shoes Shop</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.id}>
+              <a href={`/product/${product.id}`}>
+              <img
+                src={product.image}
+                alt={product.name}
+              />
+              </a>
+              <div className="productInfo">
+                <a href={`/product/${product.id}`}>
+                <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Comprar!</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            id: 1,
+            name: 'Nike sb',
+            category: 'casual',
+            image:'/img/p1.jpg',
+            price: 120,
+            countInStock:10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'bom tenis'
+
+        },
+        {   
+            id: 2,
+            name: 'DC Penza',
+            category: 'casual',
+            image:'/img/p2.jpg',
+            price: 350,
+            countInStock:2,
+            brand: 'DC',
+            rating: 4.8,
+            numReviews: 15,
+            description: 'otimo tenis'
+
+        },
+        {   
+            id: 3,
+            name: 'Air Force 1',
+            category: 'casual',
+            image:'/img/p3.png',
+            price: 700,
+            countInStock:15,
+            brand: 'Nike',
+            rating: 4.7,
+            numReviews: 10,
+            description: 'Tenis bonito'
+
+        },
+        {
+            id: 4,
+            name: 'Yeezy',
+            category: 'casual',
+            image:'/img/p4.png',
+            price: 900,
+            countInStock:2,
+            brand: 'Adidas',
+            rating: 4.7,
+            numReviews: 10,
+            description: 'Tenis bonito'
+
+        },
+    ],
+};
+
+export default data;
